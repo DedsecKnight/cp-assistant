ca9f82c814de26adb3fcce80062ce15afe329ca9
Mike Nguyen <mike.nguyen@acmutd.co>
2022-04-10 17:07:01 -0500

    chore: refactored POTW register command to have better welcome message and allow message-pinning

--- src/commands/potw/register.command.ts
+++ src/commands/potw/register.command.ts
@@ -22,11 +22,20 @@ export default class RegisterChannelCommand implements ICommand<Message> {
     args: string[]
   ): Promise<any> {
     this.utilsService.subscribeChannel(message.channel);
-    return this.messageService.sendEmbedMessage(message.channel, {
-      title: "Channel registered",
-      description:
-        "This channel is successfully registered to the POTW service.",
-      color: "GREEN",
-    });
+    const announcementMessage = await this.messageService.sendEmbedMessage(
+      message.channel,
+      {
+        title: "Channel registered",
+        description: `
+        This channel is successfully registered to the POTW service. \n
+        A Kattis problem will be sent to this channel at 4pm on the upcoming Wednesday. \n
+        Those that have access to this channel will have until 4pm of the Wednesday after to submit their solution. \n
+        To submit a solution, upload a file with the following name \`problemId.extension\`, where \`problemId\` is the problem ID, and \`extension\` can be either cpp, java, or py depending on language of choice. \n
+        Good luck with POTW to all participants!
+      `,
+        color: "GREEN",
+      }
+    );
+    await announcementMessage.pin();
   }
 }
